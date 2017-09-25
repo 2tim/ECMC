f88c2a391b4d5100bad6c39a35dcbec5efe96676
Kevin Evans <kevin.evans@lexisnexis.com>
2017-09-25 12:31:35 -0500

    added readme and updated data_reader.py

---
+++ README.md
@@ -0,0 +1,28 @@
+# ECMC Coding Challenge
+
+## Task
+Task 1 was chosen. The problem statement can be found in ./byte-reader/README.md
+
+Solution
+--------
+
+The solution is coded in Python. It assumes little when dealing wth the floating point debits and credits. For financial transactions this detail would need to be worked out in the business rules.
+
+Running the code
+----------------
+
+Ensure you have python3 installed:
+
+On a Mac you can install it with Homebrew:
+
+`brew install python3`
+
+Then you can run with:
+
+`git clone https://github.com/2tim/ECMC.git`
+
+`cd ECMC`
+
+`python3 data_reader.py`
+
+Results will be printed to the screen.
--- data_reader.py
+++ data_reader.py
@@ -1,9 +1,18 @@
 import struct
 import pprint
 
+'''
+See field definition size and rules in ./byte-reader/README.md
+
+The below code is intended to run on Python 3.5+
+
+TODO: Additional consideration should be taken for effectively handling floating point currency according to ECMC
+business rules.
+'''
+
 with open("byte-reader/data.dat", 'rb') as data:
-    print(data.read(4))
-    version = int(data.read(1).hex())
+    system = data.read(4)
+    version = int.from_bytes(data.read(1), 'big', signed=False)
     num_records = int.from_bytes(data.read(4), 'big', signed=False)
     records = []
     dollar_debits = 0.0
@@ -11,6 +20,7 @@ with open("byte-reader/data.dat", 'rb') as data:
     autopays = 0
     ended = 0
     special_balance = 0.0
+    # Found number of records to be
     for _ in range(num_records+1):
         record = {"enum": int(data.read(1).hex())}
         record.update(
@@ -19,26 +29,31 @@ with open("byte-reader/data.dat", 'rb') as data:
                 "uid": int.from_bytes(data.read(8), 'big', signed=False)
             }
         )
+        # ENUM 0 (Debit) and 1 (Credit) contain an extra 'amount' record.
         if record.get("enum") <= 0x01:
-            # TODO: adjust for network byte order for float
             raw_float = data.read(8)
-            # amount = str.fromhex(raw_float)
-            amount = struct.unpack('!ff ', raw_float)[0]
+            # using '!' for network byte order and 'd' for float64 conversion to double, which is a python float
+            amount = struct.unpack('!d', raw_float)[0]
             record.update({"amount": amount})
+            # Debit
             if record.get("enum") == 0x00:
                 dollar_debits += amount
                 if record.get("uid") == 2456938384156277127:
                     special_balance += amount
+            # Credit
             else:
                 dollar_credits += amount
                 if record.get("uid") == 2456938384156277127:
                     special_balance -= amount
+        # StartAutopay
         elif record.get("enum") == 0x02:
             autopays += 1
+        # EndAutopay
         elif record.get("enum") == 0x03:
             ended += 1
         records.append(record)
     print("What is the total amount in dollars of debits?")
+    #Assumes simple rounding here
     print("$%.2f" % dollar_debits)
     print("What is the total amount in dollars of credits?")
     print("$%.2f" % dollar_credits)
@@ -48,4 +63,3 @@ with open("byte-reader/data.dat", 'rb') as data:
     print("{} autopays where ended".format(ended))
     print("What is balance of user ID 2456938384156277127?")
     print("$%.2f" % special_balance)
-    # pprint.pprint(records)
